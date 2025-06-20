18b9aa0e64b01899ca48af2296211a52cf2d8e39
utsav1213 <hiraprautsav54@gmail.com>
2025-06-20 18:37:49 +0530

    authentication ui

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,14 @@
+
+import { SignInView } from '@/modules/auth/ui/views/sign-in-view';
+import React from 'react'
+
+const Page = () => {
+
+  console.log("Sign in page");
+  return (
+        <SignInView/>
+    
+  );
+}
+
+export default Page
---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+import { SignUpView } from "@/modules/auth/ui/views/sign-up-view";
+import React from "react";
+
+const Page = () => {
+  console.log("Sign up page");
+  return <SignUpView/>
+};
+
+export default Page;
